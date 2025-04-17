48eb4511fb440d5f6e01b9f90d5b20a4c488f6bd
Keshav <keshav.public07@gmail.com>
2025-04-17 15:14:21 +0530

    checkoutpage completed

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,5 @@
 export function formatCurrency(priceCents) {
     return (priceCents / 100).toFixed(2);
   }
+
+  //export default formatCurrency; // this mean i only export this function when this library is called upon
